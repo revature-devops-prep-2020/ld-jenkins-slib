1d00077ee9b9646ab4cdefe93044c73004b53356
Lawrence Dang <lawrend1@uci.edu>
2020-11-09 11:47:44 -0800

    add withKubeConfig to kubeDeploy

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,6 +1,6 @@
 import org.lawrencedang.SlackSender
 
-def call(String kubeMasterURL, context,  String credentialsId = 'kube-sa', String directory = './')
+def call(String kubeMasterURL, context,  String kubeCredentialsId = 'kube-sa', String directory = './')
 {
     SlackSender messenger = new SlackSender(this)
 
@@ -9,7 +9,10 @@ def call(String kubeMasterURL, context,  String credentialsId = 'kube-sa', Strin
         docker.image('reblank/kubectl_agent').inside('--net=host'){
             messenger.onComplete("${context.env.JOB_NAME} deployed to cluster.",
             "${context.env.JOB_NAME} failed to deploy to cluster.") {
-                sh "kubectl apply -f ${directory}"
+                withKubeConfig([credentialsId: kubeCredentialsId, serverUrl: kubeMasterURL])
+                {
+                    sh "kubectl apply -f ${directory}"
+                }
             }
         }
     }
